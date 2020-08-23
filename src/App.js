fce95f2e419e5930702ef5190d04f902d030bd16
Peter Sammon <petersammon2@gmail.com>
2020-08-23 18:41:18 -0400

    set up react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './components/Header'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Contact from './pages/Contact'
+import Checkout from './pages/Checkout'
+import Product from './pages/Product'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/contact">
+            <Header />
+            <Contact />
+          </Route>
+          <Route path="/login">
+            <Header />
+            <Login />
+          </Route>
+          <Route path="/product">
+            <Header />
+            <Product />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+        {/* lalalalalala */}
+      </div>
+    </Router>
   );
 }
 
