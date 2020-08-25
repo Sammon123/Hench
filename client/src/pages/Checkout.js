63c334c0ca1f98668661259e7a700468628780f6
Peter Sammon <petersammon2@gmail.com>
2020-08-25 04:49:04 -0400

    included card styling

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -31,6 +31,23 @@ const Checkout = () => {
                 setClientSecret(data.clientSecret);
             });
     }, []);
+    const cardStyle = {
+        style: {
+            base: {
+                color: "#32325d",
+                fontFamily: 'Arial, sans-serif',
+                fontSmoothing: "antialiased",
+                fontSize: "16px",
+                "::placeholder": {
+                    color: "#32325d"
+                }
+            },
+            invalid: {
+                color: "#fa755a",
+                iconColor: "#fa755a"
+            }
+        }
+    };
     return (
         <div>
             <h1>Checkout Page</h1>
