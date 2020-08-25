760519ee45f218d74280c0cf10edb064004d9548
Peter Sammon <petersammon2@gmail.com>
2020-08-25 04:38:24 -0400

    getting access to the stripe library using the useStripe() and useElements() hooks

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -1,4 +1,9 @@
 import React, { useState, useEffect } from 'react'
+import {
+    CardElement,
+    useStripe,
+    useElements
+} from "@stripe/react-stripe-js";
 
 const Checkout = () => {
     const [succeeded, setSucceeded] = useState(false);
@@ -6,6 +11,8 @@ const Checkout = () => {
     const [processing, setProcessing] = useState('');
     const [disabled, setDisabled] = useState(true);
     const [clientSecret, setClientSecret] = useState('');
+    const stripe = useStripe();
+    const elements = useElements();
     return (
         <div>
             <h1>Checkout Page</h1>
