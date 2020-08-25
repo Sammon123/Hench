416caacb932fb5b2310b533aab1381bddb8af90d
Peter Sammon <petersammon2@gmail.com>
2020-08-25 04:24:14 -0400

    created a payment intent with the order amount and the money

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 const stripe = require("stripe")("sk_test_51HGqIEBMDhC88miklofXo4t06W3K46eueCM1J9kmV2b2NFsunyhg00kl1qwo01MbGROwmaiNNDccjaq2cQQkSueL00mR9Vlqk9");
 
+// Create a PaymentIntent with the order amount and currency
+const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "usd"
+});
+res.send({
+    clientSecret: paymentIntent.client_secret
+});
