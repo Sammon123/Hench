8574ef0ef2d9f771d297054b04683c49a2941f15
Peter Sammon <petersammon2@gmail.com>
2020-08-25 04:35:00 -0400

    set up the initial state for the checkout

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Checkout = () => {
+    const [succeeded, setSucceeded] = useState(false);
+    const [error, setError] = useState(null);
+    const [processing, setProcessing] = useState('');
+    const [disabled, setDisabled] = useState(true);
+    const [clientSecret, setClientSecret] = useState('');
     return (
         <div>
             <h1>Checkout Page</h1>
