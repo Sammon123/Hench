6d45be2aea400efcd608f802c3cad99aec605871
Peter Sammon <petersammon2@gmail.com>
2020-08-25 05:01:42 -0400

    show a success upon completeion

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -71,6 +71,16 @@ const Checkout = () => {
                     {error}
                 </div>
             )}
+            {/* Show a success message upon completion */}
+            <p className={succeeded ? "result-message" : "result-message hidden"}>
+                Payment succeeded, see the result in your
+        <a
+                    href={`https://dashboard.stripe.com/test/payments`}
+                >
+                    {" "}
+          Stripe dashboard.
+        </a> Refresh the page to pay again.
+      </p>
         </div>
     )
 }
