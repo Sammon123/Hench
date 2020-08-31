cb85c820e2fc093f53cd8a79fef872f5033ec5f5
Peter Sammon <petersammon2@gmail.com>
2020-08-30 21:23:04 -0400

    passed props onto product component to display on the products page. updated product design

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Product.css'
 
-const Product = ({ id, title, image, price, rating }) => {
+const Product = ({ id, title, image, price }) => {
     return (
         <div className="product">
-            <p>{title}</p>
+            <div className="product-info">
+                <p>{title}</p>
+                <p className="product-price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+            </div>
+            <img src={image} alt="" />
+            <button>Add to Cart</button>
         </div>
     )
 }
--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -6,13 +6,20 @@ const Products = () => {
     return (
         <div className="home">
             <img src="https://www.inspireinkclothing.com/wp-content/uploads/IMGP4483.jpg" alt="" className="home-image" />
-
-            <Product
-                id="12321341"
-                title="The Fifth Vital"
-                price={29.00}
-                rating={5}
-                image="https://m.media-amazon.com/images/I/91rTf5TWhkL._AC_UY327_FMwebp_QL65_.jpg" />
+            <div className="home-row">
+                <Product
+                    id="12321341"
+                    title="The Fifth Vital"
+                    price={29.00}
+                    rating={5}
+                    image="https://m.media-amazon.com/images/I/91rTf5TWhkL._AC_UY327_FMwebp_QL65_.jpg" />
+                <Product
+                    id="12321341"
+                    title="The Fifth Vital"
+                    price={29.00}
+                    rating={5}
+                    image="https://m.media-amazon.com/images/I/91rTf5TWhkL._AC_UY327_FMwebp_QL65_.jpg" />
+            </div>
         </div>
     )
 }
